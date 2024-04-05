84e1334b8a622a84266385661d9170348022d51d
isabelly buge <isabellyeduardabuge@gmail.com>
2024-04-05 11:05:08 -0300

    codigo inicial da aplicacao

--- app/src/main/java/buge/isabelly/email/MainActivity.java
+++ app/src/main/java/buge/isabelly/email/MainActivity.java
@@ -2,7 +2,14 @@ package buge.isabelly.email;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        //Definindo acao do click do botao
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //obtendo dados digitados pelo usuario
+                EditText etEmail = (EditText) findViewById(R.id.etEmail);
+                String email = etEmail.getText().toString();
+
+                EditText etAssunto = (EditText) findViewById(R.id.etAssunto);
+                String assunto = etAssunto.getText().toString();
+
+                EditText etTexto = (EditText) findViewById(R.id.etTexto);
+                String texto = etTexto.getText().toString();
+
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+
+                i.setData(Uri.parse("mailto:"));
+
+                String[] emails = new String[] {email};
+                i.putExtra(Intent.EXTRA_EMAIL, emails);
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
+                i.putExtra(Intent.EXTRA_TEXT, texto);
+
+                try {
+                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                }
+                catch (ActivityNotFoundException e) {
+                    Toast.makeText(MainActivity.this, "Não há nenhum APP que posso realizar essa operação", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
