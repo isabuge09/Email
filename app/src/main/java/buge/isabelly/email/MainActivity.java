2df347f25f5589628607fad7aead166713c05e7e
isabelly buge <isabellyeduardabuge@gmail.com>
2024-04-09 08:20:03 -0300

    tarefa finalizada.

--- app/src/main/java/buge/isabelly/email/MainActivity.java
+++ app/src/main/java/buge/isabelly/email/MainActivity.java
@@ -17,35 +17,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnEnviar = (Button) findViewById(R.id.btnEnviar);
-        //Definindo acao do click do botao
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        Button btnEnviar = (Button) findViewById(R.id.btnEnviar); // obtem o botao pelo seu id
+
+        btnEnviar.setOnClickListener(new View.OnClickListener() {//Captura evento de click do botao
             @Override
-            public void onClick(View v) {
-                //obtendo dados digitados pelo usuario
-                EditText etEmail = (EditText) findViewById(R.id.etEmail);
-                String email = etEmail.getText().toString();
+            public void onClick(View v) { // metodo executado quando o botao e clicado
 
-                EditText etAssunto = (EditText) findViewById(R.id.etAssunto);
-                String assunto = etAssunto.getText().toString();
+                // obtendo dados digitados pelo usuario
+                EditText etEmail = (EditText) findViewById(R.id.etEmail);// obtem o campo de email pelo id
+                String email = etEmail.getText().toString();// obtem o texto do campo de email e converte para string
 
-                EditText etTexto = (EditText) findViewById(R.id.etTexto);
-                String texto = etTexto.getText().toString();
+                EditText etAssunto = (EditText) findViewById(R.id.etAssunto);// obtem o campo de assunto pelo id
+                String assunto = etAssunto.getText().toString();//obtem o texto do campo de assunto e converte para string
 
-                Intent i = new Intent(Intent.ACTION_SENDTO);
+                EditText etTexto = (EditText) findViewById(R.id.etTexto);// obtem o campo de texto pelo id
+                String texto = etTexto.getText().toString();//obtem o texto do campo de texto e converte para string
 
-                i.setData(Uri.parse("mailto:"));
+                Intent i = new Intent(Intent.ACTION_SENDTO);//cria uma intencao, indicando a acao a ser realizada
 
-                String[] emails = new String[] {email};
-                i.putExtra(Intent.EXTRA_EMAIL, emails);
-                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
-                i.putExtra(Intent.EXTRA_TEXT, texto);
+                i.setData(Uri.parse("mailto:"));// define que esta interessado apenas em apps que sao clientes de email
+
+                String[] emails = new String[] {email};//lista de strings que vao ser passados para o intent
+                i.putExtra(Intent.EXTRA_EMAIL, emails);//lista de strings de email
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto);//lista de strings de assunto
+                i.putExtra(Intent.EXTRA_TEXT, texto);//lista de strings de texto
 
                 try {
-                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                    startActivity(Intent.createChooser(i, "Escolha o APP"));//tenta executar intent, e da opcao de escolha dos apps disponiveis
                 }
                 catch (ActivityNotFoundException e) {
-                    Toast.makeText(MainActivity.this, "Não há nenhum APP que posso realizar essa operação", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Não há nenhum APP que posso realizar essa operação", Toast.LENGTH_LONG).show();//caso nao tenha apps disponiveis aparece uma mensagem de erro
                 }
             }
         });
